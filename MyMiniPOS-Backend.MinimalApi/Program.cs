dab36a1360bf909cac2f4762ea44251100bfbd23
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2025-01-04 21:39:49 +0630

    feat: add register endpoint

---
+++ MyMiniPOS-Backend.MinimalApi/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using MyMiniPOS_Backend.Database;
+using MyMiniPOS_Backend.Database.Repositories;
+using MyMiniPOS_Backend.Domain.Abstractions.Repositories;
+using MyMiniPOS_Backend.Domain.Abstractions.Services;
+using MyMiniPOS_Backend.Domain.Features.Auth;
+using MyMiniPOS_Backend.MinimalApi.Features.Auth;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
+{
+	if (!optionsBuilder.IsConfigured)
+	{
+		optionsBuilder.UseSqlServer();
+	}
+});
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthEndpoint();
+
+app.Run();
