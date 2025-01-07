14cfb646398393324ecda00fc61b5ac8e8d3334b
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2025-01-07 12:16:33 +0630

    feat: add Token service and createJWT

---
+++ MyMiniPOS-Backend.Domain/Abstractions/Services/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using MyMiniPOS_Backend.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMiniPOS_Backend.Domain.Abstractions.Services;
+
+public interface ITokenService
+{
+	public string CreateAccessToken(UserEntity userEntity);
+}
--- MyMiniPOS-Backend.Domain/Entities/UserEntity.cs
+++ MyMiniPOS-Backend.Domain/Entities/UserEntity.cs
@@ -1,9 +1,13 @@
-﻿namespace MyMiniPOS_Backend.Domain.Entities;
+﻿using System.Text.Json.Serialization;
+
+namespace MyMiniPOS_Backend.Domain.Entities;
 
 public class UserEntity
 {
+	[JsonPropertyName("id")]
 	public string Id { get; set; }
+	[JsonPropertyName("username")]
 	public string Username { get; set; }
-	public string Email { get; set; }
+	[JsonPropertyName("password")]
 	public string Password { get; set; }
 }
